ac6eef982a581bc846fcdac6d95f73813680e0df
SeanONeill005 <osean31@gmail.com>
2025-11-25 19:49:33 +0000

    Refactor: Increased readability. Proper tabbing and spaces and clearer if's,for's and while's. This code needed it

--- 16_SFML_Games/tetris.cpp
+++ 16_SFML_Games/tetris.cpp
@@ -26,9 +26,18 @@ int figures[7][4] =
 
 bool check()
 {
-   for (int i=0;i< blockPerShapeNum;i++)
-      if (blockHoriz[i].x<0 || blockHoriz[i].x>= width || blockHoriz[i].y>= height) return 0;
-      else if (field[blockHoriz[i].y][blockHoriz[i].x]) return 0;
+    for (int i = 0;i < blockPerShapeNum;i++)
+    {
+        if (blockHoriz[i].x < 0 || blockHoriz[i].x >= width || blockHoriz[i].y >= height)
+        {
+            return 0;
+        }
+        else if (field[blockHoriz[i].y][blockHoriz[i].x])
+        {
+            return 0;
+        }
+        
+    }
 
    return 1;
 };
@@ -47,8 +56,10 @@ int tetris()
 
     Sprite sprite(blockSprite), background(backgroundSprite), frame(frameSprite);
 
-    int shapePosition=0; bool rotate=0; int colorNum=1;
-    float timer=0,delay=0.3; 
+    int shapePosition=0; 
+    bool rotate=0; 
+    int colorNum=1;
+    float timer=0, delay=0.3; 
 
     Clock clock;
 
@@ -56,72 +67,119 @@ int tetris()
     {
         float time = clock.getElapsedTime().asSeconds();
         clock.restart();
-        timer+=time;
+        timer += time;
 
-        Event e;
-        while (window.pollEvent(e))
+        Event event;
+        while (window.pollEvent(event))
         {
-            if (e.type == Event::Closed)
+            if (event.type == Event::Closed)
+            {
                 window.close();
+            }
+                
 
-            if (e.type == Event::KeyPressed)
-              if (e.key.code==Keyboard::Up) rotate=true;
-              else if (e.key.code==Keyboard::Left) shapePosition=-1;
-              else if (e.key.code==Keyboard::Right) shapePosition=1;
+            if (event.type == Event::KeyPressed)
+            {
+                if (event.key.code == Keyboard::Up) 
+                {
+                    rotate = true;
+                }
+                else if (event.key.code == Keyboard::Left) 
+                {
+                    shapePosition = -1;
+                }
+                else if (event.key.code == Keyboard::Right) 
+                {
+                    shapePosition = 1;
+                }
+            }
+
         }
 
-    if (Keyboard::isKeyPressed(Keyboard::Down)) delay=0.05;
-
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+        {
+            delay = 0.05;
+        }
     //// <- Move -> ///
-    for (int i=0;i< blockPerShapeNum;i++)  { blockVertic[i]=blockHoriz[i]; blockHoriz[i].x+=shapePosition; }
-    if (!check()) for (int i=0;i< blockPerShapeNum;i++) blockHoriz[i]=blockVertic[i];
+    for (int i=0;i< blockPerShapeNum;i++)  
+    { 
+        blockVertic[i] = blockHoriz[i]; 
+        blockHoriz[i].x += shapePosition; 
+    }
+    if (!check())  
+    {
+        for (int i = 0;i < blockPerShapeNum;i++)
+        {
+            blockHoriz[i] = blockVertic[i];
+        }
+    }
 
     //////Rotate//////
     if (rotate)
-      {
+    {
         Point centerBlock = blockHoriz[1]; //center of rotation
-        for (int i=0;i< blockPerShapeNum;i++)
+        for (int i = 0;i < blockPerShapeNum; i++)
           {
             int x = blockHoriz[i].y-centerBlock.y;
             int y = blockHoriz[i].x-centerBlock.x;
             blockHoriz[i].x = centerBlock.x - x;
             blockHoriz[i].y = centerBlock.y + y;
            }
-           if (!check()) for (int i=0;i<4;i++) blockHoriz[i]=blockVertic[i];
-      }
+           if (!check())  
+           {
+               for (int i = 0;i < 4;i++)
+               {
+                   blockHoriz[i] = blockVertic[i];
+               }
+           }
+    }
 
     ///////Tick//////
     if (timer>delay)
-      {
-        for (int i=0;i< blockPerShapeNum;i++) { blockVertic[i]=blockHoriz[i]; blockHoriz[i].y+=1; }
+    {
+        for (int i = 0; i < blockPerShapeNum; i++) 
+        {
+            blockVertic[i] = blockHoriz[i]; 
+            blockHoriz[i].y += 1;
+        }
 
         if (!check())
         {
-         for (int i=0;i< blockPerShapeNum;i++) field[blockVertic[i].y][blockVertic[i].x]=colorNum;
+            for (int i=0;i< blockPerShapeNum;i++) 
+            {
+                field[blockVertic[i].y][blockVertic[i].x] = colorNum;
+            }
 
-         colorNum=1+rand()% totalColourOptions;
-         int n=rand()% totalColourOptions;
-         for (int i=0;i< blockPerShapeNum;i++)
-           {
-            blockHoriz[i].x = figures[n][i] % 2;
-            blockHoriz[i].y = figures[n][i] / 2;
-           }
+            colorNum=1+rand()% totalColourOptions;
+            int n=rand()% totalColourOptions;
+
+            for (int i=0; i < blockPerShapeNum; i++)
+            {
+                blockHoriz[i].x = figures[n][i] % 2;
+                blockHoriz[i].y = figures[n][i] / 2;
+            }
         }
 
          timer=0;
-      }
+    }
 
     ///////check lines//////////
-    int spawnPoint= height -1 ;
-    for (int i= spawnPoint;i>0;i--)
+    int spawnPoint = height - 1;
+    for (int i = spawnPoint; i > 0; i--)
     {
-        int count=0;
-        for (int j=0;j< width;j++)
+        int count = 0;
+        for (int j = 0; j < width; j++)
         {
-            if (field[i][j]) count++;
+            if (field[i][j]) 
+            {
+                count++;
+            }
             field[spawnPoint][j]=field[i][j];
         }
-        if (count< width) spawnPoint--;
+        if (count< width) 
+        {
+            spawnPoint--;
+        }
     }
 
     shapePosition=0; rotate=0; delay=0.3;
@@ -131,27 +189,33 @@ int tetris()
     window.draw(background);
           
     for (int i = 0;i < height;i++)//While the block is not at the bottom
-     for (int j=0;j< width;j++)
+    {
+        for (int j = 0;j < width;j++)
 
-       {
-         if (field[i][j]==0) continue;
-         sprite.setTextureRect(IntRect(field[i][j]* blockSpriteSize,0, blockSpriteSize, blockSpriteSize));
-         sprite.setPosition(j* blockSpriteSize,i* blockSpriteSize);
-         sprite.move(28,31); //offset
-         window.draw(sprite);
-       }
+        {
+            if (field[i][j] == 0) 
+            {
+                continue;
+            }
+            sprite.setTextureRect(IntRect(field[i][j] * blockSpriteSize, 0, blockSpriteSize, blockSpriteSize));
+            sprite.setPosition(j * blockSpriteSize, i * blockSpriteSize);
+            sprite.move(28, 31); //offset
+            window.draw(sprite);
+        }
+    }
 
-    for (int i=0;i<4;i++)
-      {
+
+    for (int i = 0; i < 4; i++)
+    {
         sprite.setTextureRect(IntRect(colorNum* blockSpriteSize,0, blockSpriteSize, blockSpriteSize));
         sprite.setPosition(blockHoriz[i].x* blockSpriteSize,blockHoriz[i].y* blockSpriteSize);
         sprite.move(28,31); //offset
         window.draw(sprite);
-      }
+    }
 
     window.draw(frame);
     window.display();
-    }
+    }//end of While(window is open)
 
     return 0;
 }
